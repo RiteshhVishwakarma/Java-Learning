6b4b356eb2945a3ea5c3abeb65ed4aeeab367c62
Ritesh Vishwakarma <113338495+RiteshhVishwakarma@users.noreply.github.com>
2025-02-11 23:34:55 +0530

    que 26

--- JavaLearning/src/arrayInJava.java
+++ JavaLearning/src/arrayInJava.java
@@ -1,12 +1,19 @@
+import java.io.PrintStream;
+
 public class arrayInJava {
     public static void main(String[] args) {
-            
+
 //        Syntax
 //        datatype[] variable_name;
 //        datetype varname[];
 
 
         int[] num = {1,2,3,4,5,6}; // initializing array
+
+        int length = num.length; // We can get the length of an array using the length property
+
+        System.out.println("Length of array is: "+length);
+        System.out.println("____________________________________________");
         System.out.println("Before Update: "+num[0]); // showing 1st Positon
 
         num[0]=540; // updating values
@@ -15,9 +22,15 @@ public class arrayInJava {
         
         for (int i=0; i<num.length; i++){
 
-            System.out.println(num[i]); // showing stored value of array
+            System.out.print(num[i]+" "); // showing stored value of array
 
         }
 
+        System.out.println("______________________________");
+
+        int intArr[] = new int[40];  // 20 memory space allocated ho jayega stack me
+        for(int i=0; i<intArr.length; i++){
+            System.out.print(intArr[i]+" ");
+        }
     }
 }
